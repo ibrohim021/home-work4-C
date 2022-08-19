06bae7a138086ebe6b58116055ceedd3c4583bf0
ibrohim021 <dim_aviasky@mail.ru>
2022-08-19 21:42:25 +0300

    exe-1

---
+++ exe-1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int Exponenta(int A, int B)
+{
+    int result = 1;
+
+    for(int i = 1; i<=B; i++)
+    {
+        result *= A;
+    }
+
+    return result;
+}
+
+Console.Write("Введите число A: ");
+var numberString = Console.ReadLine();
+int A = int.Parse(numberString!);
+
+Console.Write("Введите число B: ");
+var numberString2 = Console.ReadLine();
+int B = int.Parse(numberString2!);
+
+int exponenta = Exponenta(A, B);
+Console.WriteLine("Ответ: " + exponenta);
+
