240c6abfff39dde344e82fdbc7501b6f1c879c7d
ibrohim021 <dim_aviasky@mail.ru>
2022-08-19 22:06:37 +0300

    exe-2

---
+++ exe-2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Summ(int numberN){
+    
+    int counter = Convert.ToString(numberN).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++){
+      advance = numberN - numberN % 10;
+      result = result + (numberN - advance);
+      numberN = numberN / 10;
+    }
+   return result;
+  }
+
+Console.WriteLine("Введите числп");
+var numberString = Console.ReadLine();
+int num = int.Parse(numberString!);
+
+int result = Summ(num);
+Console.WriteLine(result);
