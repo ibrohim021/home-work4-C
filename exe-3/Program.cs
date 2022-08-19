e8d44f8c3e58296a2a80aa09c1c3be2bea927df0
ibrohim021 <dim_aviasky@mail.ru>
2022-08-19 22:22:32 +0300

    exe-3

---
+++ exe-3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] createArray()
+{
+    var random = new Random();
+    var result = new int[8];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = random.Next(0, 9);
+    }
+    return result;
+}
+
+var array = createArray();
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + ",");
+}
+
+Console.WriteLine();
+
